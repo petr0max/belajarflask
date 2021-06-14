a70f9a6866feb16756ba001cb22804c850acb9cb
Mardin Wijaya <mardinwijaya46@gmail.com>
2021-06-14 15:15:25 +0700

    initial commit

--- app/routes.py
+++ app/routes.py
@@ -6,4 +6,14 @@ from flask import render_template
 @app.route('/index')
 def index():
     user = {'username': 'udin'}
-    return render_template('index.html', title='Beranda', user=user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool'
+        }
+    ]
+    return render_template('index.html', title='Beranda', user=user, posts=posts)
